28fef13be7da55c9d0f4380b807ff618a2c29aa8
Vinayak Mishra <viks@vnykmshr.com>
2013-04-22 12:19:52 +0530

    imported code from windows box

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-twitter-scripts
-===============
+Twitter scripts
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var util = require('util');
+
+var index = {
+	available: require('./lib/available');
+};
+
+module.exports = index;
